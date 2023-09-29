fba06f24be3bd6079f69a1a8c0a35b9ed39280d0
Mateus Pereira Fernandes <mateuspereirafernandes@gmail.com>
2023-09-29 15:44:21 -0300

    tratamento dos arquivos TXT

---
+++ maquinaDeTuring.py
@@ -0,0 +1,36 @@
+with open('MT-deterministica.txt', 'r') as deterministico:
+    automato = deterministico.readlines()
+
+
+def tirar_virgula(texto):
+    virgula = ","
+    for rem in texto:
+        if rem in virgula:
+            texto = texto.replace(rem, '')
+    
+    return texto
+
+estados = automato[0]
+estados = tirar_virgula(estados)
+
+alfabeto = automato[1]
+alfabeto = tirar_virgula(alfabeto)
+
+marcadores = automato[2]
+marcadores = tirar_virgula(marcadores)
+
+
+naoFinais = automato[len(automato) -1 ]
+naoFinais = tirar_virgula(naoFinais)
+
+finais = automato[len(automato) -2]
+finais = tirar_virgula(finais)
+
+inicial = automato[len(automato) -3]
+inicial = tirar_virgula(inicial)
+
+transicoes = automato[3: (len(automato) - 4)]
+
+    
+
+print(transicoes)
